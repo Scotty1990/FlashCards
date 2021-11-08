ef67bf16c0a0f0dde0067366bc11f224ba5b204d
scottmacleod <t_mac3131@hotmail.com>
2021-11-08 14:29:52 -0600

    monday commit

--- script.js
+++ script.js
@@ -2,78 +2,112 @@ const nums = document.querySelectorAll(".numbers")
 const cards = document.querySelectorAll(".cards")
 const p1Score = document.querySelector("#p1Score")
 const winDisplay = document.querySelector("#winDisplay")
-let playerPt = 0
-/* function resetPair() {
-    if ()
-}
-*/
+const resetButton = document.querySelector("#reset")
+const p1NameInput = document.querySelector("#p1Input")
+const p1Name = document.querySelector("#p1Name")
+const submit = document.querySelector("#submit")
 
+let playerPt = 0
+let winCounter = 0
 let tempArray = []
-let arr = []
+let shownCardArray = []
 let scoreKeep = []
 let counter = 0
 let numArray = [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10]
 
-// I found out that the program doesn't stop at a setTimeout, it
-// keeps running the program and runs setTimeout a little later
+nums.forEach(num => {
+    num.style.visibility = "hidden"
+})
+
 function cardFlip() {
     setTimeout(() => {
-        arr[0].classList.toggle("numberToggle")
-        arr[1].classList.toggle("numberToggle")
-        clearArr()
+        shownCardArray[0].style.visibility = "hidden"
+        shownCardArray[1].style.visibility = "hidden"
+        clearShownCardArray()
     }, 3000)
 }
 
-function magnitude() {
+function clearTempArray() {
     tempArray.pop()
     tempArray.pop()
 }
 
-function clearArr() {
-    arr.pop()
-    arr.pop()
+function clearShownCardArray() {
+    shownCardArray.pop()
+    shownCardArray.pop()
 }
 
 function shuffleNums() {
     for (let i = numArray.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1))
         let temp = numArray[i]
-        console.log(numArray)
         numArray[i] = numArray[j]
-        console.log(numArray)
         numArray[j] = temp
-        console.log(numArray)
     }
 }
-// change newNum to nums maybe? still playing with this, remember that there are no numbers in HTML document right now
+// const newImg = document.createElement("img")
+// newImg.src = "/Users/scottmacleod/Desktop/GA/sandbox/projects/FlashCards/bart"
+// nums[1].appendChild(newImg)
+
 function setNumOrder() {
     nums.forEach((num, i) => {
         num.innerText = numArray[i]
     })
 }
+
 shuffleNums()
 setNumOrder()
 
+submit.addEventListener("click", () => {
+    p1Name.innerText = p1NameInput.value + ":"
+})
+
+resetButton.addEventListener("click", () => {
+    nums.forEach((num) => {
+        if (num.style.visibility == "visible")
+            num.style.visibility = "hidden"
+    })
+    for (let i = scoreKeep.length - 1; i >= 0; i--) {
+        scoreKeep.pop()
+    }
+    if (tempArray.length == 1) {
+        tempArray.pop()
+        counter = 0
+    }
+    if (shownCardArray.length == 1)
+        shownCardArray.pop()
+    shuffleNums()
+    setNumOrder()
+    playerPt = 0
+    p1Score.innerText = 0
+})
+
 cards.forEach((card, i) => {
     card.addEventListener("click", () => {    
-        nums[i].classList.toggle("numberToggle")
+        nums[i].style.visibility = "visible"
         tempArray.push(nums[i].innerText)
-        arr.push(nums[i])
+        shownCardArray.push(nums[i])
         counter++
         if (counter % 2 == 0) {
-            if (tempArray[0] == tempArray[1] && (scoreKeep.includes(tempArray[0]) == false && scoreKeep.includes(tempArray[1]) == false)) { // and also here
-                playerPt++
-                p1Score.innerText = " " + playerPt
-                scoreKeep.push(tempArray[0])
-                scoreKeep.push(tempArray[1])
-                clearArr()
-                if (playerPt == 10) {
-                    winDisplay.innerText = " " + 1
-                }
+            if (shownCardArray[0] == shownCardArray[1]) {
+                nums[i].style.visibility = "hidden"
+                clearShownCardArray()
             } else {
-                cardFlip()
+                if (tempArray[0] == tempArray[1] && scoreKeep.includes(tempArray[0]) == false) { // and also here
+                    playerPt++
+                    p1Score.innerText = " " + playerPt
+                    scoreKeep.push(tempArray[0])
+                    scoreKeep.push(tempArray[1])
+                    clearShownCardArray()
+                    if (playerPt == 10) {
+                        winCounter++
+                        winDisplay.innerText = " " + winCounter
+                    }
+                } else {
+                    cardFlip()
+                }
             }
-            magnitude()
+            clearTempArray()
         }
     })
 })
